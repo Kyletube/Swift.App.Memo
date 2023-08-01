0674a39231414ca4bb248f198c1dbc398bd441ac
Kyletube <kyle.pxrk@gmail.com>
2023-08-01 19:21:51 +0900

    플러스버튼추가

--- myMemo/SecondViewController.swift
+++ myMemo/SecondViewController.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUp()
         
     }
     
+    func setUp() {
+        let addButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: .none)
+        navigationItem.rightBarButtonItem = addButton
+    }
 
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
     
 
 }
