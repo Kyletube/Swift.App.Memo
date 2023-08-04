ad48b0855a814183ce48dab181730cd1f8496365
Kyletube <kyle.pxrk@gmail.com>
2023-08-04 17:10:31 +0900

    완료페이지기본틀구성

--- myMemo/ThirdViewController.swift
+++ myMemo/ThirdViewController.swift
@@ -1,29 +1,33 @@
-//
-//  ThirdViewController.swift
-//  myMemo
-//
-//  Created by Kyle on 2023/08/01.
-//
-
 import UIKit
 
 class ThirdViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ThirdViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CompleteMemoCell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
     }
-    */
-
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
 }
