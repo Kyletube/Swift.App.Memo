d533c52f6569647e176dafe8505ed4d218a0bb58
Kyletube <kyle.pxrk@gmail.com>
2023-08-04 14:46:35 +0900

    디테일뷰 바버튼삭제

--- myMemo/DetailViewController.swift
+++ myMemo/DetailViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    var memo: String?
+    var memoIndex: Int?
     var memoManager = MemoManager.shared
 
     @IBOutlet weak var memoTextView: UITextView!
@@ -14,33 +14,17 @@ class DetailViewController: UIViewController {
     }
 
     func setupUI() {
-        memoTextView.text = memo
-        memoTextView.isEditable = true
-        
-    }
-    
-    
-    
-    @IBAction func detailDeleteMemo(_ sender: UIBarButtonItem) {
-        deleteMemo()
-    }
-    
-    func deleteMemo() {
-        guard let memoToDelete = memo else { return }
-        
-        if let index = memoManager.getMemos().firstIndex(of: memoToDelete) {
-            memoManager.deleteMemo(at: index)
-            navigationController?.popViewController(animated: true)
+        if let index = memoIndex {
+            memoTextView.text = memoManager.getMemos()[index]
         }
+        memoTextView.isEditable = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
             super.viewWillDisappear(animated)
 
-            if let editedMemo = memoTextView.text {
-                if let index = memoManager.getMemos().firstIndex(of: memo!) {
-                    memoManager.updateMemo(at: index, with: editedMemo)
-                }
+            if let editedMemo = memoTextView.text, let index = memoIndex {
+                memoManager.updateMemo(at: index, with: editedMemo)
             }
         }
 }
--- myMemo/SecondViewController.swift
+++ myMemo/SecondViewController.swift
@@ -1,77 +1,93 @@
 import UIKit
 
 class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
     @IBOutlet weak var addButton: UIBarButtonItem!
     @IBOutlet weak var table: UITableView!
     var memoManager = MemoManager.shared
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUp()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            table.reloadData()
-        }
-
+        super.viewWillAppear(animated)
+        table.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memoManager.getMemos().count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MemoCell", for: indexPath)
         cell.textLabel?.text = memoManager.getMemos()[indexPath.row]
         return cell
     }
-
+    
     func setUp() {
         let addButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addButtonTapped(_:)))
         navigationItem.rightBarButtonItem = addButton
-
+        
         table.delegate = self
         table.dataSource = self
         table.register(UITableViewCell.self, forCellReuseIdentifier: "MemoCell")
         
     }
-
+    
     @IBAction func addButtonTapped(_ sender: Any) {
-        let newMemo = "새로운 메모"
-        memoManager.addMemo(newMemo)
-        table.reloadData()
+        let alertController = UIAlertController(title: "새로운 메모", message: "메모 내용을 입력하세요.", preferredStyle: .alert)
+
+            alertController.addTextField { (textField) in
+                textField.placeholder = "할 일을 입력하세요."
+            }
+
+            let addAction = UIAlertAction(title: "등록", style: .default) { (_) in
+                if let newMemo = alertController.textFields?.first?.text {
+                    self.memoManager.addMemo(newMemo)
+                    self.table.reloadData()
+                }
+            }
+
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+            alertController.addAction(addAction)
+            alertController.addAction(cancelAction)
+
+            present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true // 셀을 스와이프 가능하게 함
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            showDeleteAlert(at: indexPath)
+        }
+    }
+    
+    func showDeleteAlert(at indexPath: IndexPath) {
+        let alertController = UIAlertController(title: "메모 삭제", message: "선택한 메모를 삭제하시겠습니까?", preferredStyle: .alert)
+        
+        let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { (_) in
+            self.deleteMemo(at: indexPath)
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     func deleteMemo(at indexPath: IndexPath) {
         memoManager.deleteMemo(at: indexPath.row)
         table.reloadData()
     }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-            return true // 셀을 스와이프 가능하게 함
-        }
-
-        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-                showDeleteAlert(at: indexPath)
-            }
-        }
-    
-    func showDeleteAlert(at indexPath: IndexPath) {
-            let alertController = UIAlertController(title: "메모 삭제", message: "선택한 메모를 삭제하시겠습니까?", preferredStyle: .alert)
-            
-            let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { (_) in
-                self.deleteMemo(at: indexPath)
-            }
-            
-            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-            
-            alertController.addAction(deleteAction)
-            alertController.addAction(cancelAction)
-            
-            present(alertController, animated: true, completion: nil)
-        }
-    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "showDetail", sender: indexPath)
@@ -82,7 +98,7 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
            let detailVC = segue.destination as? DetailViewController,
            let selectedIndexPath = sender as? IndexPath {
             let selectedRow = selectedIndexPath.row
-            detailVC.memo = memoManager.getMemos()[selectedRow]
-               }
-           }
+            detailVC.memoIndex = selectedRow // memo 대신 memoIndex에 인덱스 값을 넘겨줌
+        }
+    }
 }
