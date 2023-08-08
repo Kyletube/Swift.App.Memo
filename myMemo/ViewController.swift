20dc4681f86788f886271573ba8b2c1c2eb23f06
Kyletube <kyle.pxrk@gmail.com>
2023-08-08 16:05:59 +0900

    UI변경,코드정렬

--- myMemo/ViewController.swift
+++ myMemo/ViewController.swift
@@ -2,10 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var firstViewImage: UIImageView!
-    @IBOutlet weak var checkButton: UIButton!
-    @IBOutlet weak var completeButton: UIButton!
+
     @IBOutlet weak var mainImageView: UIImageView!
+    
+    @IBOutlet weak var checkButton: UIButton!
+    
+    @IBOutlet weak var completeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,21 +17,12 @@ class ViewController: UIViewController {
     func makeUI() {
         checkButton.clipsToBounds = true
         checkButton.layer.cornerRadius = 5
-        checkButton.backgroundColor = .white
-        checkButton.setTitle("할 일 확인하기", for: .normal)
         
         completeButton.clipsToBounds = true
         completeButton.layer.cornerRadius = 5
-        completeButton.backgroundColor = .white
-        completeButton.setTitle("완료한 일 보기", for: .normal)
         
-        mainImageView.image = UIImage(named: "메모이미지")
+        mainImageView.image = UIImage(named: "MemoImage")
         
-    }
-    @IBAction func checkButtonTapped(_ sender: Any) {
-    }
-    
-    @IBAction func completeButtonTapped(_ sender: Any) {
     }
 }
 
